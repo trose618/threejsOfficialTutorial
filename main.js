7e46a6ce316f0b1ecb1e6e1309ffbdd108d0ab13
Terrance Rose Jr <terrance.j.rose@gmail.com>
2023-05-17 02:37:17 -0400

    add line

--- main.js
+++ main.js
@@ -2,11 +2,14 @@ import * as THREE from "three";
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(
-  75,
+  45,
   window.innerWidth / window.innerHeight,
-  0.1,
-  1000
+  1,
+  500
 );
+
+camera.position.set(0, 0, 100);
+camera.lookAt(0, 0, 0);
 
 const renderer = new THREE.WebGLRenderer();
 
@@ -14,12 +17,24 @@ renderer.setSize(window.innerWidth, window.innerHeight);
 
 document.body.appendChild(renderer.domElement);
 
-const geometry = new THREE.BoxGeometry(1, 1, 1);
+const geometry = new THREE.BoxGeometry(10, 10, 10);
 const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
 const cube = new THREE.Mesh(geometry, material);
 scene.add(cube);
 
-camera.position.z = 5;
+// creating line
+const mat = new THREE.LineBasicMaterial({ color: 0x00ff00 });
+
+const points = [];
+points.push(new THREE.Vector3(-10, 0, 0));
+points.push(new THREE.Vector3(0, 10, 0));
+points.push(new THREE.Vector3(10, 0, 0));
+
+const geo = new THREE.BufferGeometry().setFromPoints(points);
+
+const line = new THREE.Line(geo, mat);
+
+scene.add(line);
 
 function animate() {
   requestAnimationFrame(animate);
